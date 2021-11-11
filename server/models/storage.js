716c24658954f0f25519bf9b764ce3964b59fe1f
cnapolea <cjnapoleao@gmail.com>
2021-11-11 19:57:43 +0100

    Models: Subscription => Create a subscription model

--- server/models/storage.js
+++ server/models/storage.js
@@ -1,46 +1,49 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    maxlength: 160,
-    required: true
-  },
-  owner: {
-    type: mongoose.ObjectId,
-    required: true
-  },
-  location: {
-    type: 'Point',
-    coordinates: {
-      type: [Number],
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      maxlength: 160,
+      required: true
+    },
+    owner: {
+      type: mongoose.ObjectId,
+      required: true
+    },
+    location: {
+      type: 'Point',
+      coordinates: {
+        type: [Number],
+        required: true
+      }
+    },
+    price: { type: String, required: true },
+    galery: {
+      type: [String]
+    },
+    isRented: {
+      type: Boolean,
+      default: false
+    },
+    renter: {
+      type: mongoose.ObjectId,
+      default: null
+    },
+    width: {
+      type: Number,
+      required: true
+    },
+    length: {
+      type: Number,
       required: true
     }
   },
-  price: { type: String, required: true },
-  galery: {
-    type: [String]
-  },
-  isRented: {
-    type: Boolean,
-    default: false
-  },
-  renter: {
-    type: mongoose.ObjectId,
-    default: null
-  },
-  width: {
-    type: Number,
-    required: true
-  },
-  length: {
-    type: Number,
-    required: true
-  }
-});
+  { timestamps: { createdAt: 'createAt', updatedAt: 'updateAt' } }
+);
 
 module.exports = mongoose.model('storage', schema);
