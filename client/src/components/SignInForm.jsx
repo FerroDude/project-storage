d908791c3841457ba78db275cd2fd509c5094fcc
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-11-13 08:55:50 +0000

    removed bugged sign in form, added authentication.js file

--- client/src/components/SignInForm.jsx
+++ client/src/components/SignInForm.jsx
@@ -1,62 +1,10 @@
 import { useState } from 'react';
 
 const SignInForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      // await auth.signInWithEmailAndPassword(email, password);
-      setEmail('');
-      setPassword('');
-    } catch (error) {
-      setError(error.message);
-    }
-    setLoading(false);
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    if (name === 'email') {
-      setEmail(value);
-    } else if (name === 'password') {
-      setPassword(value);
-    }
-  };
-
   return (
-    <form className="sign-in-form" onSubmit={handleSubmit}>
-      <h2>Sign In</h2>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="error">{error}</div>
-      <button type="submit" disabled={loading}>
-        {loading ? 'Loading...' : 'Sign In'}
-      </button>
-    </form>
+    <div>
+      <h1>Sign In</h1>
+    </div>
   );
 };
-
 export default SignInForm;
--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const signUp = (body) => {
-  axios.post('/authentication/sign-up', body).then((res) => {
-    return res.data.user;
-  });
+  axios
+    .post(/*missing server here*/ '/authentication/sign-up', body)
+    .then((res) => {
+      return res.data.user;
+    });
 };
