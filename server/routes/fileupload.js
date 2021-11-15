47471b5ef7a626cd0b060f3c7a92614dc3c432a4
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-11-15 11:42:49 +0100

    implement multiple file uploads and a photo gallery component

---
+++ server/routes/fileupload.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const routeGuard = require('../middleware/route-guard');
+
+const upload = require('../config/cloudinary');
+
+const router = express.Router();
+
+router.post(
+  '/single',
+  routeGuard,
+  upload.single('profilePicture'),
+  async (req, res, next) => {
+    res.json(req.file.path);
+  }
+);
+
+router.post(
+  '/multiple',
+  routeGuard,
+  upload.array('pictures', 10), //revise the max image limit
+  async (req, res, next) => {
+    console.log(req.files);
+    const paths = req.files.map((file) => file.path);
+    res.json(paths);
+  }
+);
+
+module.exports = router;
