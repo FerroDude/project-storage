1e2c30bba5471adb68c50c3582f5bf5aa86feeb2
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-11-15 13:53:06 +0100

    implement image preview for profile pic upload

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
+
 const FileUpload = ({ type, onPickFile }) => {
+  const [previewFile, setPreviewFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    if (type === 'multiple') {
+      //   setPreviewFile(event.target.files);
+      //   onPickFile(event.target.files);
+    } else {
+      setPreviewFile(URL.createObjectURL(event.target.files[0]));
+      onPickFile(event.target.files[0]);
+      URL.revokeObjectURL(event.target.files[0]);
+    }
+  };
+
   return (
     <div>
+      <div>
+        {previewFile && <img src={previewFile} alt="preview" height="100px" />}
+      </div>
       <input
         name="fileupload"
         type="file"
         accept="image/*"
-        onChange={
-          type === 'multiple'
-            ? (event) => onPickFile(event.target.files)
-            : (event) => onPickFile(event.target.files[0])
-        }
-        multiple={type === 'multiple'}
+        onChange={handleFileChange}
       />
     </div>
   );
