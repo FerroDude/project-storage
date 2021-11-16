9f217fb6fd1bd6995ee2879a1ad9c119a46dc08e
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-11-16 17:43:23 +0000

    navigation based on user authentication, navigation as a standalone component

---
+++ client/src/components/Navigation/index.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+const Navigation = (props) => {
+  return (
+    <nav className="Navigation">
+      <Link to="/">Home</Link>
+
+      {(props.user && (
+        <>
+          <Link to="profile">Profile</Link>
+          <Link to="storage/create">Create storage</Link>
+          <Link to="/">
+            {' '}
+            <button onClick={props.handleSignOut}>Sign out</button>
+          </Link>
+        </>
+      )) || (
+        <>
+          <Link to="/signUp">Sign Up</Link>
+          <Link to="/signIn">Sign In</Link>
+        </>
+      )}
+    </nav>
+  );
+};
+
+export default Navigation;
