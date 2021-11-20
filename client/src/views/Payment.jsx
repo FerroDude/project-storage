a400a8b773ba48fb895cfcc1d08141ece767907f
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-11-20 15:40:53 +0100

    add payment form

---
+++ client/src/views/Payment.jsx
@@ -0,0 +1,81 @@
+import {
+  Elements,
+  ElementsConsumer,
+  CardElement
+} from '@stripe/react-stripe-js';
+
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      color: '#32325d',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4'
+      }
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a'
+    }
+  }
+};
+
+const NonInjectedPaymentForm = (props) => {
+  const handleFormSubmission = async (event) => {
+    event.preventDefault();
+
+    const { stripe, elements } = props;
+    const cardElement = elements.getElement(CardElement);
+
+    const result = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement
+    });
+
+    try {
+      const { paymentMethod, error } = result;
+      if (error) {
+        alert('There was an error processing your payment method details.');
+        console.log(error);
+      } else if (paymentMethod) {
+        const paymentMethodToken = paymentMethod.id;
+        this.props.onConfirmPaymentMethod(paymentMethodToken);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleFormSubmission}>
+      <CardElement options={CARD_ELEMENT_OPTIONS} />
+      <button>Confirm Subscription</button>
+    </form>
+  );
+};
+
+const STRIPE_PUBLIC_API_KEY = 'API KEY HERE!';
+
+const PaymentForm = (props) => {
+  const [stripePromise, setStripePromise] = useState(
+    loadStripe(STRIPE_PUBLIC_API_KEY)
+  );
+
+  return (
+    <Elements stripe={stripePromise}>
+      <ElementsConsumer>
+        {({ stripe, elements }) => (
+          <NonInjectedPaymentForm
+            stripe={stripe}
+            elements={elements}
+            {...props}
+          />
+        )}
+      </ElementsConsumer>
+    </Elements>
+  );
+};
+
+export default PaymentForm;
